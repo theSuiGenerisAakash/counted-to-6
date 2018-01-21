0f04a41b08e6b043010c63efa2738f0232e4af27
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-22 00:37:06 +0530

    Added more tests for restParam

--- restParam.js
+++ restParam.js
@@ -1,6 +1,8 @@
 module.exports = function average(...args) {
     let result = 0;
     let n = 0;
+    if(args.length === 0)
+        return 0.0;
     args.forEach(function (value) {
         result += value;
         n += 1;
--- test.js
+++ test.js
@@ -34,3 +34,7 @@ const restParam = require('./restParam');
 // test('Min of [-Infinity, -Infinity] should be -Infinity',() => expect(spreadOp([-Infinity, -Infinity])).toBe(-Infinity));
 
 test('Should return 3 for 1,3,5 :', () => expect(restParam(1,3,5)).toBe(3));
+test('Should return 0.0 for no input values:', () => expect(restParam()).toBe(0.0));
+test('Should return Infinity for Infinity, Infinity:', () => expect(restParam(Infinity,Infinity)).toBe(Infinity));
+test('Should return Infinity for Infinity, -3:', () => expect(restParam(Infinity,-3)).toBe(Infinity));
+test('Should return NaN for Infinity, -Infinity:', () => expect(restParam(Infinity,-Infinity)).toBe(NaN));
