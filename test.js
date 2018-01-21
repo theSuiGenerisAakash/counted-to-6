82bd7503329f90923025261ecfef69be06e070c0
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-21 23:59:01 +0530

    Added test for spreadOp.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 // const sum = require('./sumTest');
 // const templateStrings = require('./template-strings');
 // const arrowFunc = require('./arrowFunc');
-const destruct = require('./destruct');
-// const spreadOp = require('./spreadOp');
+// const destruct = require('./destruct');
+const spreadOp = require('./spreadOp');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum.sum(1, 2)).toBe(3);
@@ -17,14 +17,16 @@ const destruct = require('./destruct');
 //     expect(arrowFunc('["Fill", " ", "Umbrella"]')).toBe('[Fill, ,Umbrella] becomes "F U"');
 // });
 
-test('Argument [1, "jdoe", "jdoe@example.com", "John", "Doe"] should return jdoe and jdoe@example.com', () => {
-    expect(destruct([1, 'jdoe', 'jdoe@example.com', 'John', 'Doe'])).toEqual({username: 'jdoe', email: 'jdoe@example.com'});
-});
+// test('Argument [1, "jdoe", "jdoe@example.com", "John", "Doe"] should return jdoe and jdoe@example.com', () => {
+//     expect(destruct([1, 'jdoe', 'jdoe@example.com', 'John', 'Doe'])).toEqual({username: 'jdoe', email: 'jdoe@example.com'});
+// });
+//
+// test('Argument [1, "jdoe", "jdoe@example.com", "John"] should return jdoe and jdoe@example.com', () => {
+//     expect(destruct([1, 'jdoe', 'jdoe@example.com', 'John'])).toEqual({username: 'jdoe', email: 'jdoe@example.com'});
+// });
+//
+// test('Argument [] should return undefined', () => {
+//     expect(destruct([])).toEqual({username: undefined, email: undefined});
+// });
 
-test('Argument [1, "jdoe", "jdoe@example.com", "John"] should return jdoe and jdoe@example.com', () => {
-    expect(destruct([1, 'jdoe', 'jdoe@example.com', 'John'])).toEqual({username: 'jdoe', email: 'jdoe@example.com'});
-});
-
-test('Argument [] should return undefined', () => {
-    expect(destruct([])).toEqual({username: undefined, email: undefined});
-});
+test('Min of [0,-1,2,2] should be -1',() => expect(spreadOp([0,-1,2,2])).toBe(-1));
