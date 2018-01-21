2c976d24368090bc3d90dc870ae59c9c0321e7c5
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-21 22:32:31 +0530

    Added test for arrowFunc

---
+++ arrowFunc.js
@@ -0,0 +1,3 @@
+// var inputs = process.argv.slice(2);
+// var result = inputs.map(a => a[0]).reduce((accumulator, elem) => accumulator + elem, '');
+// console.log('[' + inputs.toString() + ']', 'becomes', '"' + result + '"');
--- test.js
+++ test.js
@@ -1,10 +1,17 @@
 // const sum = require('./sumTest');
-const templateStrings = require('./template-strings');
+// const templateStrings = require('./template-strings');
+const arrowFunc = require('./arrowFunc');
+
+
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum.sum(1, 2)).toBe(3);
 // });
 
-test('Argument "Avishek" returns Hello, Avishek! on one line and Your name lowercased is "avishek". on the second line:', () => {
-    expect(templateStrings('Avishek')).toBe(`Hello, Avishek!
-    Your name lowercased is "avishek".`);
+// test('Argument "Avishek" returns Hello, Avishek! on one line and Your name lowercased is "avishek". on the second line:', () => {
+//     expect(templateStrings('Avishek')).toBe(`Hello, Avishek!
+//     Your name lowercased is "avishek".`);
+// });
+
+test('An argument of ["Fill", " ", "Umbrella"] should return "F U":', () => {
+    expect(arrowFunc(['Fill', ' ', 'Umbrella'])).toBe('["Fill", " ", "Umbrella"] should return "F U"');
 });
