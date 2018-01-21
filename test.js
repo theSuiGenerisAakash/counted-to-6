832a76d3e2498c7ecc52304a999ac483dc6936c9
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-21 23:56:03 +0530

    Added tests for destruct

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 // const templateStrings = require('./template-strings');
 // const arrowFunc = require('./arrowFunc');
 const destruct = require('./destruct');
+// const spreadOp = require('./spreadOp');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum.sum(1, 2)).toBe(3);
@@ -16,7 +17,14 @@ const destruct = require('./destruct');
 //     expect(arrowFunc('["Fill", " ", "Umbrella"]')).toBe('[Fill, ,Umbrella] becomes "F U"');
 // });
 
-//A maximum of five arguments can be passed
 test('Argument [1, "jdoe", "jdoe@example.com", "John", "Doe"] should return jdoe and jdoe@example.com', () => {
     expect(destruct([1, 'jdoe', 'jdoe@example.com', 'John', 'Doe'])).toEqual({username: 'jdoe', email: 'jdoe@example.com'});
+});
+
+test('Argument [1, "jdoe", "jdoe@example.com", "John"] should return jdoe and jdoe@example.com', () => {
+    expect(destruct([1, 'jdoe', 'jdoe@example.com', 'John'])).toEqual({username: 'jdoe', email: 'jdoe@example.com'});
+});
+
+test('Argument [] should return undefined', () => {
+    expect(destruct([])).toEqual({username: undefined, email: undefined});
 });
