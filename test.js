88490c8076d691e969d24b9fe11d7e2224b5ac85
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-21 23:27:30 +0530

    Added test for destruct.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // const sum = require('./sumTest');
 // const templateStrings = require('./template-strings');
 // const arrowFunc = require('./arrowFunc');
-const arrowFunc2 = require('./arrowFunc2');
+const destruct = require('./destruct');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum.sum(1, 2)).toBe(3);
@@ -15,3 +15,7 @@ const arrowFunc2 = require('./arrowFunc2');
 // test('An argument of ["Fill", " ", "Umbrella"] should return "F U":', () => {
 //     expect(arrowFunc('["Fill", " ", "Umbrella"]')).toBe('[Fill, ,Umbrella] becomes "F U"');
 // });
+
+test('Argument [1, "jdoe", "jdoe@example.com", "John", "Doe"] should return jdoe and jdoe@example.com', () => {
+    expect(destruct([1, 'jdoe', 'jdoe@example.com', 'John', 'Doe'])).toEqual(['jdoe', 'jdoe@example.com']);
+});
