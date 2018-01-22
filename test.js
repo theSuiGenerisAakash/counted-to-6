d75e080f8d87b5fcc4f418e9efb93fdff9c63dfd
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-22 07:55:46 +0530

    added one more test for makeImp.js

--- test.js
+++ test.js
@@ -50,3 +50,7 @@ const makeImp = require('./makeImp');
 test('An empty string should be returing an empty string', () => {
     expect(makeImp('')).toBe('');
 });
+//Always test before committing
+test('An empty string with 5 should be returing !!!!!', () => {
+    expect(makeImp('', 5)).toBe('!!!!!');
+});
